481c0620499d5b48aad850cdd709c3c009e3e393
uche-inyama <depoulet@yahoo.com>
2021-12-14 04:49:18 +0100

    set routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/'
+import MovieDetail from './containers/MovieDetail'
+
 
 const App = ()  => {
   return (
     <div className="App">
-     
+     <Router>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/movie/:imdbID' element={<MovieDetail />}/>
+      </Routes>
+      <Footer />
+     </Router>
     </div>
   );
 }
