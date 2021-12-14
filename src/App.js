1f09a17486b62e37eed6aaf66a745a7918cd3d7c
uche-inyama <depoulet@yahoo.com>
2021-12-14 05:22:02 +0100

    fix error setting up router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './components/'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home'
 import MovieDetail from './containers/MovieDetail'
 
 
 const App = ()  => {
   return (
     <div className="App">
-     <Router>
-      <Header />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/movie/:imdbID' element={<MovieDetail />}/>
-      </Routes>
-      <Footer />
-     </Router>
+      <Router>
+        <Header />
+          <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/movies/:imdbID' element={<MovieDetail />}/>
+          </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
