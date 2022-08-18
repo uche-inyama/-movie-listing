0144d9318a49763bc10473a1c837891ad95ce964
uche-inyama <depoulet@yahoo.com>
2022-08-18 12:53:52 +0000

    update url to http(s)

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export const fetchMovies = async (args) => {
-  const res = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${args}&type=movie`);
+  const res = await axios.get(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${args}&type=movie`);
   return res
 }
 
 export const fetchShows = async (args) => {
-  const res =  await axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${args}&type=series`)
+  const res =  await axios.get(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${args}&type=series`)
   return res.data
 }
 
 export const fetchMovieDetail = async (imdbID) => {
-  const res =  await axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${imdbID}&plot=full`)
+  const res =  await axios.get(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${imdbID}&plot=full`)
   return res.data
 }
 
